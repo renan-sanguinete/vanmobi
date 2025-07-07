f1abdb0c79bec9d1fdd6b375f8233aae2ade45fd
Renan Franca <renansf82@gmail.com>
2025-07-07 18:34:48 -0300

    Add escopo inicial de pastas

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,11 @@
-plugins: ["nativewind/babel"]
+module.exports = function (api) {
+    api.cache(true);
+    return {
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
+    };
+  };
+  
+  
